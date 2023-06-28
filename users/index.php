0b00c9d7a4b4169875c2ae56c62d49b8e18822c7
Hom bdr. pathak <pathakhom17@gmail.com>
2023-06-28 15:22:05 +0545

    Index.php colors and some imp. Comments are mentioned

--- users/index.php
+++ users/index.php
@@ -1,29 +1,47 @@
 <?php
 session_start();
 error_reporting(0);
+
+//Include the configuration files which likely contains database connection detalils 
 include("includes/config.php");
+
+//Check if the form is submitted
 if(isset($_POST['submit']))
 {
+
+	//Retrieve the user details from the database based on the entered email and password
 $ret=mysqli_query($bd, "SELECT * FROM users WHERE userEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
 $num=mysqli_fetch_array($ret);
+
+
 if($num>0)
 {
+
+	//If the user is found on the database, set session variables and log the user in
 $extra="change-password.php";//
 $_SESSION['login']=$_POST['username'];
 $_SESSION['id']=$num['id'];
 $host=$_SERVER['HTTP_HOST'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=1;
+
+//Insert the log entry for the user login
 $log=mysqli_query($bd, "insert into userlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['login']."','$uip','$status')");
+
+//Redirect user to a different page after successful login
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
 exit();
+
 }
 else
 {
+	//If the user is not found on the database, handle the unsuccessful login attempt by displaying an error msg
 $_SESSION['login']=$_POST['username'];	
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=0;
+
+//Insert a log entry for the unsuccessful login attempt
 mysqli_query($bd, "insert into userlog(username,userip,status) values('".$_SESSION['login']."','$uip','$status')");
 $errormsg="Invalid username or password";
 $extra="login.php";
@@ -31,21 +49,28 @@ $extra="login.php";
 }
 }
 
+//Check if the password change form is submitted
 if(isset($_POST['change']))
 {
    $email=$_POST['email'];
     $contact=$_POST['contact'];
     $password=md5($_POST['password']);
+
+	//Check if the email and contact number are valid in the database
 $query=mysqli_query($bd, "SELECT * FROM users WHERE userEmail='$email' and contactNo='$contact'");
 $num=mysqli_fetch_array($query);
+
+
 if($num>0)
 {
+	//If the email and contact number are valid, update the user's password
 mysqli_query($bd, "update users set password='$password' WHERE userEmail='$email' and contactNo='$contact' ");
 $msg="Password Changed Successfully";
 
 }
 else
 {
+	//If the email and contact number are not valid, display an error message
 $errormsg="Invalid email id or Contact no";
 }
 }
@@ -110,7 +135,7 @@ return true;
 		                    <a data-toggle="modal" href="login.html#myModal"> Forgot Password?</a>
 		                </span>
 		            </label>
-		            <button class="btn btn-theme btn-block" name="submit" type="submit" style="background-color:#CB6CE6"><i class="fa fa-lock"></i> SIGN IN</button>
+		            <button class="btn btn-theme btn-block" name="submit" type="submit" style="background-color:#8C52FF"><i class="fa fa-lock"></i> SIGN IN</button>
 		            <hr>
 		    		</form>
 		            <div class="registration">
