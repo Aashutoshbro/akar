f857b7632768d0e0a486dc2f58832d4913eae4bd
Aashubro <aashutoshbro1@gmail.com>
2023-07-09 19:42:55 +0545

    completed includes/header.php properly aligned nav icon and button and added php

--- users/includes/header.php
+++ users/includes/header.php
@@ -1,15 +1,15 @@
 <!-- This will only run when used header.php file in another files -->
 
-<header class="header black-bg"  style="background:#3970b3;height:64px;">
+<header class="header black-bg"  style="background:#3970b3;">
     <div class="sidebar-toggle-box" style="color:#fff;">
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
     </div>
     <!--logo start-->
-    <a href="index.html" class="logo"><b> Road Maintenance Complaint System</b></a>
+    <a href="index.html" class="logo" style="margin:16px 0 0 10px;"><b> Road Maintenance Complaint System</b></a>
                 
     <div class="top-menu">
         <ul class="nav pull-right top-menu">
-            <li><a class="logout" href="logout.php"  style="background:#CB6CE6;font-weight:bold;font-size:18px;width:90px;height:35px;">Logout</a></li>
+            <li><a class="logout" href="logout.php"  style="background:#CB6CE6;font-size:16px;font-weight:bold;width:80px;height:32px;">Logout</a></li>
         </ul>
     </div>
 </header>
