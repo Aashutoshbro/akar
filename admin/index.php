315c56e633c2974dfdb14cc018372603761c1d6a
Aashubro <aashutoshbro1@gmail.com>
2023-09-22 12:42:11 +0545

    customize admin page and authenticated session

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 error_reporting(0);
@@ -30,10 +31,12 @@ exit();
 }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>CMS | Admin login</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -41,16 +44,112 @@ exit();
 	<link type="text/css" href="admin/css/theme.css" rel="stylesheet">
 	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
 	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
-</head>
-<body>
+    <style>
+        html {
+        height: 100%;
+        }
+        body {
+        margin:0;
+        padding:0;
+        font-family: sans-serif;
+        background: linear-gradient(#141e30, #243b55);
+        }
+
+        .login-box {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 400px;
+        padding: 40px;
+        transform: translate(-50%, -50%);
+        background: rgba(0,0,0,.5);
+        box-sizing: border-box;
+        box-shadow: 0 15px 25px rgba(0,0,0,.6);
+        border-radius: 10px;
+        }
+
+        .login-box h2 {
+        margin: 0 0 30px;
+        padding: 0;
+        color: #fff;
+        text-align: center;
+        }
+
+        .login-box .user-box {
+        position: relative;
+        }
+
+        .login-box .user-box input {
+        width: 100%;
+        padding: 10px 0;
+        font-size: 16px;
+        color: #fff;
+        margin-bottom: 30px;
+        border: none;
+        border-bottom: 1px solid #fff;
+        outline: none;
+        background: transparent;
+        }
+        .login-box .user-box label {
+        position: absolute;
+        top:0;
+        left: 0;
+        padding: 10px 0;
+        font-size: 16px;
+        color: #fff;
+        pointer-events: none;
+        transition: .5s;
+        }
+
+        .login-box .user-box input:focus ~ label,
+        .login-box .user-box input:valid ~ label {
+        top: -20px;
+        left: 0;
+        color: #03e9f4;
+        font-size: 12px;
+        }
+
+        .login-box form a {
+        position: relative;
+        display: inline-block;
+        padding: 10px 20px;
+        color: #03e9f4;
+        font-size: 16px;
+        text-decoration: none;
+        text-transform: uppercase;
+        overflow: hidden;
+        transition: .5s;
+        margin-top: 40px;
+        letter-spacing: 4px
+        }
+
+        .login-box a:hover {
+        background: #03e9f4;
+        color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 5px #03e9f4,
+                    0 0 25px #03e9f4,
+                    0 0 50px #03e9f4,
+                    0 0 100px #03e9f4;
+        }
+        .bg{
+            background:#3970b3;
+            height:44px;
+        }
+        
+    </style>
+    </head>
+    <body>
+    
+
 	<div class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
+		<div class="navbar-inner bg">
 			<div class="container">
 				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
 					<i class="icon-reorder shaded"></i>
 				</a>
 
-			  	<a class="brand" href="index.html">
+			  	<a class="brand" href="http://localhost/akar/" style="color:white;">
 			  		AKAR CMS | Admin
 			  	</a>
 
@@ -58,14 +157,10 @@ exit();
 				
 					<ul class="nav pull-right">
 
-						<li><a href="http://localhost/Complaint Management System/">
+						<li><a href="http://localhost/akar/" style="color:white;">
 						Back to Portal
-						
-						</a></li>
+						</a></li>	
 
-						
-
-						
 					</ul>
 				</div><!-- /.nav-collapse -->
 			</div>
@@ -73,48 +168,24 @@ exit();
 	</div><!-- /navbar -->
 
 
+    <div class="login-box">
+        <h2>Admin Login</h2>
+    
+        <form method="post">
+        <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+            <div class="user-box">
+                <input class="span12" type="text" id="inputEmail" name="username" placeholder="Username" required="" >
+            </div>
+            <div class="user-box">
+                <input class="span12" type="password" id="inputPassword" name="password" placeholder="Password" required="" >
+            </div>
+            <div class="controls clearfix">
+                <button type="submit" class="btn btn-primary pull-right" name="submit">Login</button>
+            </div>
 
-	<div class="wrapper">
-		<div class="container">
-			<div class="row">
-				<div class="module module-login span4 offset4">
-					<form class="form-vertical" method="post">
-						<div class="module-head">
-							<h3>Sign In</h3>
-						</div>
-						<span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
-						<div class="module-body">
-							<div class="control-group">
-								<div class="controls row-fluid">
-									<input class="span12" type="text" id="inputEmail" name="username" placeholder="Username">
-								</div>
-							</div>
-							<div class="control-group">
-								<div class="controls row-fluid">
-						<input class="span12" type="password" id="inputPassword" name="password" placeholder="Password">
-								</div>
-							</div>
-						</div>
-						<div class="module-foot">
-							<div class="control-group">
-								<div class="controls clearfix">
-									<button type="submit" class="btn btn-primary pull-right" name="submit">Login</button>
-									
-								</div>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div><!--/.wrapper-->
+        </form>
+    </div>
 
-	<div class="footer">
-		<div class="container">
-			<b class="copyright">&copy; 2023 CMS </b>AKAR All rights reserved.
-		</div>
-	</div>
-	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-</body>
+
+  </body>
+</html>
