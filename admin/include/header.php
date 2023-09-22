d041804e034d8836da40d2979220431ada933a6f
Aashubro <aashutoshbro1@gmail.com>
2023-09-22 12:45:04 +0545

    link for admin changed

--- admin/include/header.php
+++ admin/include/header.php
@@ -6,7 +6,7 @@
 					<i class="icon-reorder shaded"></i>
 				</a>
 
-			  	<a class="brand" href="index.html">
+			  	<a class="brand" href="http://localhost/akar/">
 			  		AKAR CMS | Admin
 			  	</a>
 
